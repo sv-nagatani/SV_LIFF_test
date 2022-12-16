c622c9896364120b038eaada7907ef71b34323ac
sv_nagatani <yu.nagatani@softventure.co.jp>
2022-12-16 13:19:23 +0900

    [fix]LIFF SDKの読み込みを即時実行するよう修正

--- src/main/resources/static/js/liffinit.js
+++ src/main/resources/static/js/liffinit.js
@@ -1,19 +1,17 @@
 /**
  * LIFFの初期化
  */
-$(function() {
-	liff
-	.init({ liffId: '1657646587-vq0dZBwj' })
-	.then(() => {
-		console.log("Success! you can do something with LIFF API here.")
-	})
-	.catch((error) => {
-		console.log(error)
-	});
-	if(liff.isInClient() && !liff.isLoggedIn()) {
-		liff.login();
-	}
-	else {
-		console.log("Not in LINE Browser Or Logged In")
-	}
+liff
+.init({ liffId: '1657646587-vq0dZBwj' })
+.then(() => {
+	console.log("Success! you can do something with LIFF API here.")
+})
+.catch((error) => {
+	console.log(error)
 });
+if(liff.isInClient() && !liff.isLoggedIn()) {
+	liff.login();
+}
+else {
+	console.log("Not in LINE Browser Or Logged In")
+}
